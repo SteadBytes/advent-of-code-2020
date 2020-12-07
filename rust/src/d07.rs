467540a3dd5521bf651edbe77d7c024164ee444d
Ben Steadman <steadmanben1@gmail.com>
2020-12-07 19:20:16 +0000

    [rust] day 07 part 01 quick-n-dirty
    Signed-off-by: Ben Steadman <steadmanben1@gmail.com>

--- rust/src/d07.rs
+++ rust/src/d07.rs
@@ -1,5 +1,4 @@
 #![allow(dead_code)]
-
 use std::collections::{HashMap, VecDeque};
 
 const MY_BAG: &str = "shiny gold";
@@ -42,14 +41,55 @@ fn part_1(rules: &BagRules) -> usize {
 
 fn part_2() {}
 
-fn parse_input(input: &str) -> Result<BagRules, ParseError> {
-    todo!()
-}
-
 pub fn run(input: &str) {
     let rules = parse_input(input).expect("unable to parse input");
     println!("Part 1: {}", part_1(&rules));
     // println!("Part 2: {}", part_2(&parsed));
+}
+
+/// Rough syntax for puzzle input:
+/// ```text
+/// rules = rule [{ "\n" rule }]
+/// rule = bag " bags contain " ["no other bags." | rule-items "."]
+/// rule-items = { rule-item ", " }
+/// rule-item = { digit } " " bag "bag" [ "s" ]
+/// bag = { letter } " " { letter }
+/// ```
+fn parse_input(input: &str) -> Result<BagRules, ParseError> {
+    // FIXME: Actually return error cases
+    Ok(input
+        .lines()
+        .map(|line| {
+            let (l, r) = split_once(line, " contain ").unwrap();
+            let (_, parent) = rsplit_once(l, " ").unwrap();
+            if r == "no other bags." {
+                (parent, vec![])
+            } else {
+                let children = r.trim_end_matches(".").split(", ").map(|child| {
+                    let (quantity, rest) = split_once(child, " ").unwrap();
+                    let (_, bag) = rsplit_once(rest, " ").unwrap();
+                    (bag, quantity.parse::<usize>().unwrap())
+                });
+                (parent, children.collect())
+            }
+        })
+        .collect())
+}
+
+fn split_once<'a>(s: &'a str, pat: &'a str) -> Option<(&'a str, &'a str)> {
+    let mut split = s.splitn(2, pat);
+    match (split.next(), split.next()) {
+        (Some(s1), Some(s2)) => Some((s1, s2)),
+        _ => None,
+    }
+}
+
+fn rsplit_once<'a>(s: &'a str, pat: &'a str) -> Option<(&'a str, &'a str)> {
+    let mut split = s.rsplitn(2, pat);
+    match (split.next(), split.next()) {
+        (Some(s1), Some(s2)) => Some((s1, s2)),
+        _ => None,
+    }
 }
 
 #[derive(PartialEq, PartialOrd, Eq, Ord, Debug)]
@@ -68,8 +108,7 @@ shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
 dark olive bags contain 3 faded blue bags, 4 dotted black bags.
 vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
 faded blue bags contain no other bags.
-dotted black bags contain no other bags.
-";
+dotted black bags contain no other bags.";
 
     #[test]
     fn parse_input_example() {
@@ -96,24 +135,7 @@ dotted black bags contain no other bags.
 
     #[test]
     fn part_1_example() {
-        /* let rules = parse_input(EXAMPLE_INPUT).unwrap(); */
-
-        let rules: BagRules = vec![
-            ("light red", vec![("bright white", 1), ("muted yellow", 2)]),
-            (
-                "dark orange",
-                vec![("bright white", 3), ("muted yellow", 4)],
-            ),
-            ("bright white", vec![("shiny gold", 1)]),
-            ("muted yellow", vec![("shiny gold", 2), ("faded blue", 9)]),
-            ("shiny gold", vec![("dark olive", 1), ("vibrant plum", 2)]),
-            ("dark olive", vec![("faded blue", 3), ("dotted black", 4)]),
-            ("vibrant plum", vec![("faded blue", 5), ("dotted black", 6)]),
-            ("faded blue", vec![]),
-            ("dotted black", vec![]),
-        ]
-        .into_iter()
-        .collect();
+        let rules = parse_input(EXAMPLE_INPUT).unwrap();
         assert_eq!(part_1(&rules), 4);
     }
 
