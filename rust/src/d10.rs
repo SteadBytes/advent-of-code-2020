eb2ebd2f809f74aa9636709fa5cbcc969bd75971
Ben Steadman <steadmanben1@gmail.com>
2020-12-11 19:45:25 +0000

    [rust] day 10 part 02
    Signed-off-by: Ben Steadman <steadmanben1@gmail.com>

--- rust/src/d10.rs
+++ rust/src/d10.rs
@@ -1,3 +1,5 @@
+use std::cmp::min;
+
 /// The puzzle description is, I suspect, deliberately convoluted. Since *every* adapter must be
 /// used and adapter can only connect to another that is 1-3 jolts lower itself there is only
 /// a single valid configuration - sorted in ascending order.
@@ -22,7 +24,35 @@ fn part_1(ratings: &Vec<usize>) -> Result<usize, Error> {
     Ok(diff_1 * diff_3)
 }
 
-fn part_2() {}
+/// Dynamic programming approach based on the following observations:
+/// - Not *all* the adapters need to be used
+/// - First and last adapters (0 and max(ratings) + 3) are always in the same position (e.g.
+///   1 valid arrangement)
+/// - Each adapter has *at most* 3 others it can connect to e.g. there exists adapters with ratings
+///   +1, +2 and +3 from it. It's not possible to have more than this otherwise there would be no
+///   valid arrangements of all the adapters.
+///
+/// Assumes `ratings` is *already* sorted.
+fn part_2(ratings: &Vec<usize>) -> Result<usize, Error> {
+    let n = ratings.len();
+    // Holds cumulative number of arrangements for each adapter - counts[i] is the number
+    // of valid arrangements for adapters in ratings[i..n]. At the end of the loop
+    // below, counts[0] contains the number of arrangements for all adapters.
+    let mut counts = vec![0; n];
+    // 1 arrangement of all adapters (sorted order)
+    counts[n - 1] = 1;
+
+    for (i, x) in ratings.iter().enumerate().rev().skip(1) {
+        // Search the 3 possible other adapters
+        let (lo, hi) = (i + 1, min(i + 3, n - 1)); // Stop at index 0
+        counts[i] = (lo..=hi) // Indices of possible other adapters
+            .filter(|&j| ratings[j] - x <= 3) // Valid connections
+            .map(|j| counts[j]) // Number of arrangments for other adapters
+            .sum() // Total arrangements for this adapter
+    }
+
+    Ok(counts[0])
+}
 
 fn parse_input(input: &str) -> Result<Vec<usize>, Error> {
     let mut ratings: Vec<usize> = input
@@ -41,7 +71,7 @@ pub fn run(input: &str) {
         "Part 1: {}",
         part_1(&ratings).expect("invalid adapter difference encountered")
     );
-    // println!("Part 2: {}", part_2(&parsed));
+    println!("Part 2: {}", part_2(&ratings).unwrap()); // FIXME
 }
 
 #[derive(PartialEq, Eq, Debug)]
@@ -133,5 +163,14 @@ mod tests {
     }
 
     #[test]
-    fn part_2_example() {}
+    fn part_2_example_1() {
+        let ratings = parse_input(EXAMPLE_INPUT_1).unwrap();
+        assert_eq!(part_2(&ratings).unwrap(), 8);
+    }
+
+    #[test]
+    fn part_2_example_2() {
+        let ratings = parse_input(EXAMPLE_INPUT_2).unwrap();
+        assert_eq!(part_2(&ratings).unwrap(), 19208);
+    }
 }
