6485377b14c72874505d8971ce3710c459a30a17
Ben Steadman <steadmanben1@gmail.com>
2020-12-13 12:20:15 +0000

    [rust] day 12 clean up w/type safety
    Signed-off-by: Ben Steadman <steadmanben1@gmail.com>

--- rust/src/d12.rs
+++ rust/src/d12.rs
@@ -1,5 +1,11 @@
 use std::convert::TryFrom;
+use std::ops;
 use Instruction::*;
+
+// TODO: There is probably some  abstraction to be had out of `part_1` and `part_2`. They only
+// differ in which entity is updated by each instruction (ship direction, ship location or waypoint
+// location). I'm  not forcing it for the sake of reducing code size, however, as this will
+// convolute the logic for each part.
 
 fn part_1(instructions: &Vec<Instruction>) -> u32 {
     let mut pos = (0, 0);
@@ -16,8 +22,8 @@ fn part_1(instructions: &Vec<Instruction>) -> u32 {
             South(n) => pos.1 -= n,
             East(n) => pos.0 += n,
             West(n) => pos.0 -= n,
-            Left(n) => dir = clockwise_rotate(&dir, -*n),
-            Right(n) => dir = clockwise_rotate(&dir, *n),
+            Left(n) => dir = (-*n).apply_to(&dir),
+            Right(n) => dir = n.apply_to(&dir),
         }
     }
 
@@ -38,8 +44,8 @@ fn part_2(instructions: &Vec<Instruction>) -> u32 {
             South(n) => wp.1 -= n,
             East(n) => wp.0 += n,
             West(n) => wp.0 -= n,
-            Left(n) => wp = clockwise_rotate(&wp, -*n),
-            Right(n) => wp = clockwise_rotate(&wp, *n),
+            Left(n) => wp = (-*n).apply_to(&wp),
+            Right(n) => wp = n.apply_to(&wp),
         }
     }
 
@@ -63,8 +69,8 @@ fn parse_input(input: &str) -> Result<Vec<Instruction>, ParseError> {
                     "S" => Ok(South(n)),
                     "E" => Ok(East(n)),
                     "W" => Ok(West(n)),
-                    "L" => Ok(Left(n)), // FIXME: Validate angle
-                    "R" => Ok(Right(n)),
+                    "L" => Ok(Left(TurnAngle::new(n).ok_or(ParseError::InvalidAngle(n))?)),
+                    "R" => Ok(Right(TurnAngle::new(n).ok_or(ParseError::InvalidAngle(n))?)),
                     _ => Err(ParseError::InvalidCommand(cmdpart)),
                 }
             }
@@ -78,14 +84,36 @@ pub fn run(input: &str) {
     println!("Part 2: {}", part_2(&instructions));
 }
 
-fn clockwise_rotate(p: &(i32, i32), angle: i32) -> (i32, i32) {
-    // Since puzzle is represented using unit grid, angles should only be multiples of 90
-    match angle {
-        0 => *p,
-        90 | -270 => (p.1, -p.0),
-        180 | -180 => (-p.0, -p.1),
-        270 | -90 => (-p.1, p.0),
-        _ => panic!("unexpected angle"), // FIXME: Constrain this via enum at parse time?
+/// Newtype representing a valid angle for a `Left` or `Right` instruction. Values should only be
+/// multiples of `+-90` as the puzzle is represented using unit grid.
+#[derive(Debug, PartialEq, PartialOrd, Eq, Ord, Copy, Clone)]
+struct TurnAngle(i32);
+
+impl TurnAngle {
+    fn new(angle: i32) -> Option<TurnAngle> {
+        match angle {
+            0 | 90 | -90 | -180 | 180 | -280 | 270 => Some(TurnAngle(angle)),
+            _ => None,
+        }
+    }
+
+    /// Returns `p` rotated *clockwise* by `self`.
+    fn apply_to(&self, p: &(i32, i32)) -> (i32, i32) {
+        match self.0 {
+            0 => *p,
+            90 | -270 => (p.1, -p.0),
+            180 | -180 => (-p.0, -p.1),
+            270 | -90 => (-p.1, p.0),
+            _ => unreachable!(),
+        }
+    }
+}
+
+impl ops::Neg for TurnAngle {
+    type Output = Self;
+
+    fn neg(self) -> Self::Output {
+        Self(-self.0)
     }
 }
 
@@ -95,8 +123,8 @@ enum Instruction {
     South(i32),
     East(i32),
     West(i32),
-    Left(i32),
-    Right(i32),
+    Left(TurnAngle),
+    Right(TurnAngle),
     Forward(i32),
 }
 
@@ -105,6 +133,7 @@ enum ParseError<'a> {
     InvalidInstruction(&'a str),
     InvalidCommand(&'a str),
     InvalidValue(std::num::ParseIntError),
+    InvalidAngle(i32),
 }
 
 #[cfg(test)]
@@ -122,7 +151,13 @@ F11";
     fn parse_input_example() {
         assert_eq!(
             parse_input(EXAMPLE_INPUT).unwrap(),
-            [Forward(10), North(3), Forward(7), Right(90), Forward(11)]
+            [
+                Forward(10),
+                North(3),
+                Forward(7),
+                Right(TurnAngle::new(90).unwrap()),
+                Forward(11)
+            ]
         );
     }
 
