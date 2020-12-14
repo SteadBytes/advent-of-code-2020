9c980ab2ca1c13f1268960933ce8f73fd3e8f3f3
Ben Steadman <steadmanben1@gmail.com>
2020-12-14 07:21:08 +0000

    [rust] day 13 refactor input parsing to support part 2
    For part 01, "x"s in the schedule were completely ignored and thus
    filtered out of the input as soon as possible during parsing. These
    values are significant for part 02 however so this commit changes the
    data structure parsed from the input and the part 01 logic to handle
    this.
    
    Signed-off-by: Ben Steadman <steadmanben1@gmail.com>

--- rust/src/d13.rs
+++ rust/src/d13.rs
@@ -1,7 +1,9 @@
-fn part_1(earliest_timestamp: u32, bus_ids: &Vec<u32>) -> u32 {
-    let mut best_bus: Option<u32> = None;
-    let mut min_wait = u32::MAX;
-    for id in bus_ids {
+type Schedule = Vec<(usize, u64)>;
+
+fn part_1(earliest_timestamp: u64, schedule: &Schedule) -> u64 {
+    let mut best_bus: Option<u64> = None;
+    let mut min_wait = u64::MAX;
+    for (_, id) in schedule {
         let wait_time = id - (earliest_timestamp % id);
         if wait_time < min_wait {
             min_wait = wait_time;
@@ -14,28 +16,34 @@ fn part_1(earliest_timestamp: u32, bus_ids: &Vec<u32>) -> u32 {
 
 fn part_2() {}
 
-fn parse_input(input: &str) -> Result<(u32, Vec<u32>), ParseError> {
+fn parse_input(input: &str) -> Result<(u64, Vec<(usize, u64)>), ParseError> {
     let mut lines = input.lines();
     let timestamp = lines
         .next()
         .ok_or(ParseError::MissingTimestamp)?
-        .parse::<u32>()
+        .parse::<u64>()
         .map_err(|e| ParseError::InvalidTimestamp(e))?;
-    let bus_ids = lines
+    let schedule = lines
         .next()
         .ok_or(ParseError::MissingBusIds)?
         .split(",")
-        .filter(|s| *s != "x")
-        .map(|s| s.parse::<u32>().map_err(|e| ParseError::InvalidBusId(e)))
-        .collect::<Result<Vec<u32>, ParseError>>()?;
+        .enumerate()
+        .filter(|(_, s)| *s != "x")
+        .map(|(i, s)| {
+            Ok((
+                i,
+                s.parse::<u64>().map_err(|e| ParseError::InvalidBusId(e))?,
+            ))
+        })
+        .collect::<Result<Vec<(usize, u64)>, ParseError>>()?;
 
-    Ok((timestamp, bus_ids))
+    Ok((timestamp, schedule))
 }
 
 pub fn run(input: &str) {
-    let (earliest_timestamp, bus_ids) = parse_input(input).expect("unable to parse input");
-    println!("Part 1: {}", part_1(earliest_timestamp, &bus_ids));
-    // println!("Part 2: {}", part_2(&parsed));
+    let (earliest_timestamp, schedule) = parse_input(input).expect("unable to parse input");
+    println!("Part 1: {}", part_1(earliest_timestamp, &schedule));
+    // println!("Part 2: {}", part_2(&schedule));
 }
 
 #[derive(PartialEq, Eq, Debug)]
@@ -58,14 +66,14 @@ mod tests {
     fn parse_input_example() {
         assert_eq!(
             parse_input(EXAMPLE_INPUT).unwrap(),
-            (939, vec![7, 13, 59, 31, 19])
+            (939, vec![(0, 7), (1, 13), (4, 59), (6, 31), (7, 19)])
         );
     }
 
     #[test]
     fn part_1_example() {
-        let (ts, bus_ids) = parse_input(EXAMPLE_INPUT).unwrap();
-        assert_eq!(part_1(ts, &bus_ids), 295);
+        let (ts, schedule) = parse_input(EXAMPLE_INPUT).unwrap();
+        assert_eq!(part_1(ts, &schedule), 295);
     }
 
     #[test]
