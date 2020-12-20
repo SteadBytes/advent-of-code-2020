d0e813f92aeed0b0a20cde50c54a8a044327229e
Ben Steadman <steadmanben1@gmail.com>
2020-12-20 15:30:00 +0000

    [rust] day 17 part 02 (needs cleanup, no time right now)
    part_2 is almost complete duplication of part_1. I intend to refactor
    via some abstraction over the two coordinate types e.g. a HasNeighbours
    trait to provide the neighbours() function. part_1 and part_2 can then
    just call a function abstracted over HasNeighbours implementors that does
    all the logic.
    
    Signed-off-by: Ben Steadman <steadmanben1@gmail.com>

--- rust/src/d17.rs
+++ rust/src/d17.rs
@@ -2,26 +2,8 @@ use crate::char_enum;
 use std::collections::HashSet;
 use CubeState::*;
 
-#[derive(PartialEq, PartialOrd, Eq, Ord, Debug, Hash, Clone, Copy)]
-struct Coord3(i64, i64, i64);
-
-impl Coord3 {
-    // TODO: return impl Iterator<Item = Coord3>
-    fn neighbours(&self) -> Vec<Coord3> {
-        let mut r = Vec::with_capacity(26);
-        for dx in -1..=1 {
-            for dy in -1..=1 {
-                for dz in -1..=1 {
-                    if dx == 0 && dy == 0 && dz == 0 {
-                        continue;
-                    }
-                    r.push(Coord3(self.0 + dx, self.1 + dy, self.2 + dz))
-                }
-            }
-        }
-        r
-    }
-}
+// FIXME: Remove major duplication from part_1 and part_2 as they differ in their type
+// signature (Coord3 vs Coord4).
 
 /// Observation: Only need to keep track of the coordinates containing active cubes.
 ///
@@ -70,7 +52,7 @@ fn part_1(active_cubes: &HashSet<Coord3>) -> usize {
 
         for c in inactive.iter() {
             // Count active neighbours & update next_state accordingly
-            let active_neighbours = c.neighbours().iter().filter(|n| state.contains(n)).count();
+            let active_neighbours = c.neighbours().filter(|n| state.contains(n)).count();
 
             // Apply rule 2:
             // > If a cube is inactive but exactly 3 of its neighbors are active, the cube
@@ -85,7 +67,52 @@ fn part_1(active_cubes: &HashSet<Coord3>) -> usize {
     state.len()
 }
 
-fn part_2() {}
+/// Exactly the same as `part_1`, just using 4 dimensions.
+fn part_2(active_cubes: &HashSet<Coord4>) -> usize {
+    let mut state = active_cubes.clone();
+    for _ in 0..6 {
+        let mut next_state = HashSet::new(); // TODO: Avoid this?
+        let mut inactive = HashSet::new(); // TODO: Avoid this?
+        for c in state.iter() {
+            // Iterate through neighbours
+            // - Update inactive set
+            // - Count active neighbours
+            // Update next_state according to active_neighbours
+            let mut active_neighbours = 0;
+            for n in c.neighbours() {
+                if state.contains(&n) {
+                    active_neighbours += 1;
+                } else {
+                    inactive.insert(n);
+                }
+            }
+
+            // Apply rule 1:
+            // > If a cube is active and exactly 2 or 3 of its neighbors are also active,
+            // > the cube remains active. Otherwise, the cube becomes inactive.
+            if active_neighbours == 2 || active_neighbours == 3 {
+                next_state.insert(*c);
+            } else {
+                inactive.insert(*c);
+            }
+        }
+
+        for c in inactive.iter() {
+            // Count active neighbours & update next_state accordingly
+            let active_neighbours = c.neighbours().filter(|n| state.contains(n)).count();
+
+            // Apply rule 2:
+            // > If a cube is inactive but exactly 3 of its neighbors are active, the cube
+            // > becomes active. Otherwise, the cube remains inactive.
+            if active_neighbours == 3 {
+                next_state.insert(*c);
+            }
+        }
+        // Update all cubes simulataneously
+        state = next_state;
+    }
+    state.len()
+}
 
 fn parse_input(input: &str) -> Result<HashSet<Coord3>, ParseError> {
     input
@@ -108,9 +135,47 @@ fn parse_input(input: &str) -> Result<HashSet<Coord3>, ParseError> {
 }
 
 pub fn run(input: &str) {
-    let initial_state = parse_input(input).expect("unable to parse input");
-    println!("Part 1: {}", part_1(&initial_state));
-    // println!("Part 2: {}", part_2(&parsed));
+    let initial_state_3d = parse_input(input).expect("unable to parse input");
+    println!("Part 1: {}", part_1(&initial_state_3d));
+
+    let initial_state_4d = initial_state_3d
+        .iter()
+        .map(|c| Coord4::from_3d(c))
+        .collect();
+    println!("Part 2: {}", part_2(&initial_state_4d));
+}
+
+#[derive(PartialEq, PartialOrd, Eq, Ord, Debug, Hash, Clone, Copy)]
+struct Coord3(i64, i64, i64);
+
+impl<'a> Coord3 {
+    fn neighbours(&'a self) -> impl Iterator<Item = Coord3> + 'a {
+        (-1..=1)
+            .flat_map(move |dx| (-1..=1).flat_map(move |dy| (-1..=1).map(move |dz| (dx, dy, dz))))
+            .filter(|x| *x != (0, 0, 0))
+            .map(move |(dx, dy, dz)| Coord3(self.0 + dx, self.1 + dy, self.2 + dz))
+    }
+}
+
+#[derive(PartialEq, PartialOrd, Eq, Ord, Debug, Hash, Clone, Copy)]
+struct Coord4(i64, i64, i64, i64);
+
+impl<'a> Coord4 {
+    fn from_3d(c: &Coord3) -> Self {
+        Self(c.0, c.1, c.2, 0)
+    }
+
+    fn neighbours(&'a self) -> impl Iterator<Item = Coord4> + 'a {
+        // TODO: Surely there's a better way to do this...
+        (-1..=1)
+            .flat_map(move |dx| {
+                (-1..=1).flat_map(move |dy| {
+                    (-1..=1).flat_map(move |dz| (-1..=1).map(move |dw| (dx, dy, dz, dw)))
+                })
+            })
+            .filter(|x| *x != (0, 0, 0, 0))
+            .map(move |(dx, dy, dz, dw)| Coord4(self.0 + dx, self.1 + dy, self.2 + dz, self.3 + dw))
+    }
 }
 
 char_enum! {
@@ -156,5 +221,13 @@ mod tests {
     }
 
     #[test]
-    fn part_2_example() {}
+    fn part_2_example() {
+        let initial_state_3d = parse_input(EXAMPLE_INPUT).unwrap();
+        let initial_state_4d: HashSet<Coord4> = initial_state_3d
+            .iter()
+            .map(|c| Coord4::from_3d(c))
+            .collect();
+
+        assert_eq!(part_2(&initial_state_4d), 848);
+    }
 }
