e84b638f191b23bc05884f2f9445220e8c478da2
Ben Steadman <steadmanben1@gmail.com>
2020-12-29 08:09:53 +0000

    FIXME ROUGH day 23 part 02
    Mostly copy/paste of part 01 with minor changes to cup label integer
    sizes (u8 -> u32). This actually works and only takes a second or so to
    solve (thanks rust) but there is definitely a more efficient solution.
    Looking at it, I'm not sure that the elaborate HashMap/LinkedList data
    structure is necessary and is *certainly* hurting performance. Is there
    another way to model links between labels without *physically*
    representing the links as pointers to nodes?
    
    Signed-off-by: Ben Steadman <steadmanben1@gmail.com>

--- rust/src/d23.rs
+++ rust/src/d23.rs
@@ -2,7 +2,7 @@ use std::cell::RefCell;
 use std::collections::HashMap;
 use std::rc::{Rc, Weak};
 
-type Cups = HashMap<u8, Rc<RefCell<ListNode>>>;
+type Cups = HashMap<u32, Rc<RefCell<ListNode>>>;
 
 /// Key points:
 /// - **Clockwise** circular list of cups
@@ -51,7 +51,8 @@ type Cups = HashMap<u8, Rc<RefCell<ListNode>>>;
 ///     - As nodes can be removed from the LL but *not* dropped, their next pointers may not be
 ///       present at any given time -> use `Option`.
 /// Simple simulation
-fn part_1(mut cups: Cups, starting_cup: u8) -> String {
+fn part_1(cup_labels: &[u32]) -> String {
+    let (cups, starting_cup) = build_cups(cup_labels.iter().copied()); // FIXME
     let mut current_cup = Rc::clone(cups.get(&starting_cup).unwrap());
     for _ in 0..100 {
         // "Remove" 3 cups
@@ -93,7 +94,16 @@ fn part_1(mut cups: Cups, starting_cup: u8) -> String {
         current_cup = next;
     }
 
-    // FIXME
+    let traversal = cups_after_1(&cups);
+    // FIXME: There's definitely a better way to do this
+    traversal
+        .iter()
+        .map(|label| format!("{}", label))
+        .collect::<Vec<_>>()
+        .join("")
+}
+
+fn cups_after_1(cups: &Cups) -> Vec<u32> {
     let mut traversal = vec![];
     let mut current = cups.get(&1).unwrap().borrow().next_node();
     loop {
@@ -104,68 +114,126 @@ fn part_1(mut cups: Cups, starting_cup: u8) -> String {
         }
         current = Rc::clone(&next);
     }
-    // FIXME: There's definitely a better way to do this
     traversal
-        .iter()
-        .map(|label| format!("{}", label))
-        .collect::<Vec<_>>()
-        .join("")
 }
 
-fn part_2() {
-    todo!()
+/// Unable to find an exploitable pattern in the cup movements. Let's try the brute force
+/// full simulation approach of `part_1` just on a *much* larger scale...
+///
+/// Brute force: Extend `cups` to `1000000` cups and run `part_1` algorithm  `10000000` times.
+fn part_2(cup_labels: &[u32]) -> u64 {
+    let (cups, starting_cup) = build_cups(
+        cup_labels
+            .iter()
+            .copied()
+            .chain((cup_labels.iter().max().unwrap() + 1)..=1000000),
+    );
+    let max_label = 1000000;
+    let mut current_cup = Rc::clone(cups.get(&starting_cup).unwrap());
+    for _ in 0..10000000 {
+        // "Remove" 3 cups
+        let c1 = current_cup.borrow().next_node();
+        let c2 = c1.borrow().next_node();
+        let c3 = c2.borrow().next_node();
+
+        // Choose destination cup
+        // Since nodes are stored in HashMap by label, we don't actually need to traverse
+        // the LL. Instead, decrement the destination label value so long as it is equal to either
+        // the current cup label or any of the 3 removed cup labels. Wrap around to the maximum
+        // label value if 0 is hit.
+        let destination_cup = {
+            let mut destination_label = current_cup.borrow().label;
+            while destination_label == current_cup.borrow().label
+                || destination_label == c1.borrow().label
+                || destination_label == c2.borrow().label
+                || destination_label == c3.borrow().label
+            {
+                destination_label -= 1;
+                if destination_label == 0 {
+                    destination_label = max_label;
+                }
+            }
+            Rc::clone(cups.get(&destination_label).unwrap())
+        };
+
+        // "Insert" removed cups after destination cup
+        // Update current cup next pointer to third removed cup next pointer
+        current_cup.borrow_mut().next = Some(Weak::clone(&c3.borrow().next.as_ref().unwrap()));
+        // Update destination cup and third removed cup next pointers
+        c3.borrow_mut().next = Some(Weak::clone(
+            &destination_cup.borrow().next.as_ref().unwrap(),
+        ));
+        destination_cup.borrow_mut().next = Some(Rc::downgrade(&c1));
+        // Select new current cup
+        let next = current_cup.borrow().next_node();
+        current_cup = next;
+    }
+
+    let n1 = cups.get(&1).unwrap().borrow().next_node().borrow().label;
+    let n2 = cups
+        .get(&1)
+        .unwrap()
+        .borrow()
+        .next_node()
+        .borrow()
+        .next_node()
+        .borrow()
+        .label;
+
+    n1 as u64 * n2 as u64
 }
 
-fn parse_input(input: &str) -> Result<(Cups, u8), ParseError> {
-    let cup_labels: Vec<u8> = input
-        .lines()
-        .next()
-        .ok_or(ParseError::InvalidInput)?
-        .chars()
-        .map(|d| {
-            d.to_digit(10)
-                // Parsed successfully as base 10 -> safe cast to u8
-                .map(|x| x as u8)
-                .ok_or(ParseError::InvalidLabel(d))
-        })
-        .collect::<Result<_, _>>()?;
-
+fn build_cups<T>(cup_labels: T) -> (Cups, u32)
+where
+    T: Iterator<Item = u32>,
+{
+    let mut cup_labels = cup_labels.into_iter();
     let mut m = HashMap::new();
+    let first_label = cup_labels.next().unwrap();
     // Insert the first cup
     let head = Rc::new(RefCell::new(ListNode {
-        label: cup_labels[0],
+        label: first_label,
         next: None,
     }));
-    m.insert(cup_labels[0], head);
+    m.insert(first_label, head);
 
     // Build nodes for remaining cups
-    let mut prev_node = Rc::clone(m.get(&cup_labels[0]).unwrap());
-    for l in &cup_labels[1..] {
+    let mut prev_node = Rc::clone(m.get(&first_label).unwrap());
+    for l in cup_labels {
         let new_node = Rc::new(RefCell::new(ListNode {
-            label: *l,
+            label: l,
             next: None,
         }));
         prev_node.borrow_mut().next = Some(Rc::downgrade(&new_node));
         prev_node = Rc::clone(&new_node);
-        m.insert(*l, new_node);
+        m.insert(l, new_node);
     }
 
     // Link last cup to first cup to ensure circular list
-    m.get(cup_labels.last().unwrap()).unwrap().borrow_mut().next =
-        Some(Rc::downgrade(m.get(&cup_labels[0]).unwrap()));
+    prev_node.borrow_mut().next = Some(Rc::downgrade(m.get(&first_label).unwrap()));
 
-    Ok((m, cup_labels[0]))
+    (m, first_label)
+}
+
+fn parse_input(input: &str) -> Result<Vec<u32>, ParseError> {
+    input
+        .lines()
+        .next()
+        .ok_or(ParseError::InvalidInput)?
+        .chars()
+        .map(|d| d.to_digit(10).ok_or(ParseError::InvalidLabel(d)))
+        .collect()
 }
 
 pub fn run(input: &str) {
-    let (cups, starting_cup) = parse_input(input).expect("unable to parse input");
-    println!("Part 1: {}", part_1(cups, starting_cup));
-    // println!("Part 2: {}", part_2(&parsed));
+    let cup_labels = parse_input(input).expect("unable to parse input");
+    println!("Part 1: {}", part_1(&cup_labels));
+    println!("Part 2: {}", part_2(&cup_labels));
 }
 
 #[derive(Debug)]
 struct ListNode {
-    label: u8,
+    label: u32, // Part 2 requires `u32` range for labels
     next: Option<Weak<RefCell<ListNode>>>,
 }
 
@@ -190,23 +258,30 @@ mod tests {
 
     #[test]
     fn parse_input_example() {
-        let expected_labels = [3, 8, 9, 1, 2, 5, 4, 6, 7];
+        assert_eq!(
+            parse_input(EXAMPLE_INPUT).unwrap(),
+            [3, 8, 9, 1, 2, 5, 4, 6, 7]
+        );
+    }
 
-        let (cups, starting_cup) = parse_input(EXAMPLE_INPUT).unwrap();
+    #[test]
+    fn build_cups_example() {
+        let cup_labels = parse_input(EXAMPLE_INPUT).unwrap();
+        let (cups, starting_cup) = build_cups(cup_labels.iter().copied());
 
         assert_eq!(
-            starting_cup, expected_labels[0],
+            starting_cup, cup_labels[0],
             "starting cup was not the first cup in the input"
         );
 
         assert_eq!(
             cups.len(),
-            expected_labels.len(),
+            cup_labels.len(),
             "incorrect number of cups parsed from input"
         );
 
         // Check node values match HashMap keys
-        for label in &expected_labels {
+        for label in &cup_labels {
             assert_eq!(
                 cups.get(label).unwrap().borrow().label,
                 *label,
@@ -227,18 +302,18 @@ mod tests {
             }
             current = Rc::clone(&next);
         }
-        assert_eq!(traversal, expected_labels, "LL traversal order incorrect");
+        assert_eq!(traversal, cup_labels, "LL traversal order incorrect");
     }
 
     #[test]
     fn part_1_example() {
-        let (cups, starting_cup) = parse_input(EXAMPLE_INPUT).unwrap();
-        assert_eq!(part_1(cups, starting_cup), "67384529");
+        let cup_labels = parse_input(EXAMPLE_INPUT).unwrap();
+        assert_eq!(part_1(&cup_labels), "67384529");
     }
 
-    // #[test]
-    // fn part_2_example() {
-    //     let parsed = parse_input(EXAMPLE_INPUT).unwrap();
-    //     assert_eq!(part_2(&parsed),);
-    // }
+    #[test]
+    fn part_2_example() {
+        let cup_labels = parse_input(EXAMPLE_INPUT).unwrap();
+        assert_eq!(part_2(&cup_labels), 149245887792);
+    }
 }
