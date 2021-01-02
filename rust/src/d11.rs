e2bd9b46ea201f3a58e83f2191bfac73d73d3bc9
Ben Steadman <steadmanben1@gmail.com>
2021-01-02 16:17:23 +0000

    [rust] day 11 part 02
    Finally got round to coming back to this one. Not a big challenge, just
    got behind due to being busy during the main contest and wanted to keep
    going ahead.
    
    Plenty of room for cleanup/de-duplication (I've left a TODO or three)
    - no time atm though - only 1 more star left to get now!
    
    Signed-off-by: Ben Steadman <steadmanben1@gmail.com>

--- rust/src/d11.rs
+++ rust/src/d11.rs
@@ -2,6 +2,14 @@ use std::fmt;
 use Location::*;
 use SeatState::*;
 
+// TODO: Plenty of room for cleanup/de-duplication between part_1 and part_2 (mainly in various
+// methods on Grid) but this works and executes v fast on my laptop.
+
+/// Key points:
+/// - Rules based on adjacent seats:
+///     - Empty w/ `0` adjacent occupied -> occupied
+///     - Occupied w/ `>=4` visible occupied seats -> empty
+///     - Floor never changes
 fn part_1(layout: &mut Grid) -> usize {
     // TODO: Limit this to prevent infinite loop in the case of malformed input?
     loop {
@@ -11,8 +19,36 @@ fn part_1(layout: &mut Grid) -> usize {
     }
 }
 
+/// Key points:
+/// - Rules based on **visible seats** instead of adjacent
+///     - Empty w/ 0 visible occupied -> occupied
+///     - Occupied w/ `>=5` visible occupied seats -> empty
+///     - Floor never changes
+/// - Visible seats = first seat within line of sight of each direction
+///
+/// `4` visible occupied seats:
+///
+/// ```text
+/// #...|..#/
+/// .\.#|../.
+/// .#\.|./..
+/// ...\|/...
+/// ..#-L---#
+/// .../|\...
+/// ../.|.\..
+/// #/..#..\.
+/// /..#....\
+/// ```
+/// "Ray casting" approach:
+/// - Generate coordinates for each location in a "ray" by following the points along it's vector
+///     - Repeatedly add the unit vector of the ray to the current coordinate
+/// - Traverse the ray coordinates until a visible seat is found or the boundary of the grid is reached
 fn part_2(layout: &mut Grid) -> usize {
-    todo!()
+    loop {
+        if !layout.tick_v2() {
+            return layout.count_occupied();
+        }
+    }
 }
 
 fn parse_input(input: &str) -> Result<Grid, ParseError> {
@@ -44,7 +80,7 @@ fn parse_input(input: &str) -> Result<Grid, ParseError> {
 pub fn run(input: &str) {
     let layout = parse_input(input).expect("unable to parse input");
     println!("Part 1: {}", part_1(&mut layout.clone()));
-    // println!("Part 2: {}", part_2(&parsed));
+    println!("Part 2: {}", part_2(&mut layout.clone()));
 }
 
 #[derive(PartialEq, PartialOrd, Eq, Ord, Debug, Clone, Copy)]
@@ -68,9 +104,20 @@ struct Grid {
 }
 
 impl Grid {
+    const ADJACENT_DIRS: [(i8, i8); 8] = [
+        (-1, -1),
+        (-1, 0),
+        (-1, 1),
+        (0, -1),
+        (0, 1),
+        (1, -1),
+        (1, 0),
+        (1, 1),
+    ];
+
     /// Seat transition rules:
-    /// - Empty & no adjacent occupied -> occupied
-    /// - Occupied & >= 4 adjacent occupied -> empty
+    /// - Empty & `0` adjacent occupied -> occupied
+    /// - Occupied `& >= 4` adjacent occupied -> empty
     /// - Otherwise -> no change
     fn tick(&mut self) -> bool {
         let mut g_next = self.g.clone();
@@ -99,23 +146,44 @@ impl Grid {
         change
     }
 
+    /// Seat transition rules:
+    /// - Empty & `0` visible occupied -> occupied
+    /// - Occupied `& >= 5` visible occupied -> empty
+    /// - Otherwise -> no change
+    fn tick_v2(&mut self) -> bool {
+        let mut g_next = self.g.clone();
+        let mut change = false;
+
+        self.g
+            .iter()
+            .enumerate()
+            .filter_map(|(i, l)| match l {
+                Floor => None,
+                Seat(state) => Some((i, state)),
+            })
+            .for_each(|(i, s)| match (s, self.visible_occupied(i)) {
+                (Empty, 0) => {
+                    change = true;
+                    g_next[i] = Seat(Occupied);
+                }
+                (Occupied, x) if x >= 5 => {
+                    change = true;
+                    g_next[i] = Seat(Empty)
+                }
+                _ => (),
+            });
+
+        self.g = g_next;
+        change
+    }
+
     fn count_occupied(&self) -> usize {
         self.g.iter().filter(|l| **l == Seat(Occupied)).count()
     }
 
     fn adjacent_occupied(&self, idx: usize) -> usize {
-        const ADJACENT_DIRS: [(i8, i8); 8] = [
-            (-1, -1),
-            (-1, 0),
-            (-1, 1),
-            (0, -1),
-            (0, 1),
-            (1, -1),
-            (1, 0),
-            (1, 1),
-        ];
         let (x, y) = self.pos(idx).unwrap(); // FIXME
-        ADJACENT_DIRS
+        Grid::ADJACENT_DIRS
             .iter()
             .filter_map(|(dx, dy)| {
                 // Using Option::and_then to filter out-of-bounds locations e.g. (-1, -1)
@@ -126,6 +194,33 @@ impl Grid {
                     .and_then(|i| self.g.get(i))
             })
             .filter(|l| **l == Location::Seat(Occupied))
+            .count()
+    }
+
+    fn visible_occupied(&self, idx: usize) -> usize {
+        let (x, y) = self.pos(idx).unwrap(); // FIXME
+        Grid::ADJACENT_DIRS
+            .iter()
+            .filter_map(|(dx, dy)| {
+                // Repeatedly apply dx, dy until seat is found or out of bounds
+                let mut ray = (1..)
+                    .map(|i| {
+                        let x1 = checked_add(x, *dx * i);
+                        let y1 = checked_add(y, *dy * i);
+                        x1.zip(y1)
+                            .and_then(|(x, y)| self.loc(x, y))
+                            .and_then(|i| self.g.get(i))
+                    })
+                    // Edge of grid reached
+                    .take_while(|x| x.is_some());
+                // Follow ray until *first* seat
+                ray.find(|l| match l {
+                    Some(Seat(_)) => true,
+                    _ => false,
+                })
+                .and_then(|l| Some(l.unwrap()))
+            })
+            .filter(|l| **l == Seat(Occupied))
             .count()
     }
 
@@ -323,5 +418,68 @@ L.LLLLL.LL";
     fn part_2_example() {
         let mut layout = parse_input(EXAMPLE_INPUT).unwrap();
         assert_eq!(part_2(&mut layout), 26);
+    }
+
+    #[test]
+    fn visible_occupied() {
+        let test_cases = [
+            (
+                "\
+.......#.
+...#.....
+.#.......
+.........
+..#L....#
+....#....
+.........
+#........
+...#.....",
+                (3, 4),
+                8,
+            ),
+            (
+                "\
+.............
+.............
+.L.L.#.#.#.#.
+.............",
+                (2, 2),
+                0,
+            ),
+            (
+                "\
+.##.##.
+#.#.#.#
+##...##
+...L...
+##...##
+#.#.#.#
+.##.##.",
+                (3, 3),
+                0,
+            ),
+            (
+                "\
+#.LL.LL.L#
+#LLLLLL.LL
+L.L.L..L..
+L#LL.LL.#L
+L.#L.LL.#L
+L.#LLL#.#L
+..L.#.....
+LLLLLLLLL#
+#.LLLLLL.L
+#.LLLLL.L#",
+                (0, 0),
+                1,
+            ),
+        ];
+        for (input, (x, y), expected) in &test_cases {
+            let layout = parse_input(&input).unwrap();
+            assert_eq!(
+                layout.visible_occupied(layout.loc(*x, *y).unwrap()),
+                *expected
+            );
+        }
     }
 }
